4af8b9ebc8402a446672bb51ba946b238a44bd35
JustinDosaj <justindosaj@gmail.com>
2025-08-02 13:43:22 -0700

    Initial Commit

---
+++ terraform/env/dev/main.tf
@@ -0,0 +1,17 @@
+provider "aws" {
+    region = var.region
+    profile = var.aws-profile
+
+    default_tags {
+        tags = {
+            Environment = var.environment
+            Project = "social-media"
+        }
+    }
+}
+
+
+module "cognito_module" {
+    source = "../../modules/cognito"
+    environment = var.environment
+}
---
+++ terraform/modules/cognito/main.tf
@@ -0,0 +1,42 @@
+resource "aws_cognito_user_pool" "social_media_pool" {
+    name = "${var.environment}-social-media-pool"
+
+    auto_verified_attributes = ["email"]
+    alias_attributes = ["email"]
+
+    schema {
+      name = "email"
+      attribute_data_type = "String"
+      required = true
+    }
+
+    password_policy {
+        minimum_length      = 8
+        require_lowercase   = false
+        require_numbers     = false
+        require_symbols     = false
+        require_uppercase   = false
+    }
+
+    email_configuration {
+        email_sending_account = "COGNITO_DEFAULT"
+    }   
+}
+
+resource "aws_cognito_user_pool_client" "social-media-pool-client" {
+  name = "${var.environment}-social-media-pool-client"
+  user_pool_id = aws_cognito_user_pool.social_media_pool.id
+
+  explicit_auth_flows = [
+    "ALLOW_USER_SRP_AUTH",
+    "ALLOW_REFRESH_TOKEN_AUTH",
+    "ALLOW_USER_PASSWORD_AUTH"
+  ]
+
+  supported_identity_providers = ["COGNITO"]
+  callback_urls                = ["your-app://callback"]
+  logout_urls                  = ["your-app://logout"]
+  allowed_oauth_flows          = ["code"]
+  allowed_oauth_scopes         = ["email", "openid", "profile"]
+  generate_secret              = false
+}
