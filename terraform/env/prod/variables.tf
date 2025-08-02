68dffb663fe9f4d548acdf21e6bef12c5e6fc7fa
JustinDosaj <justindosaj@gmail.com>
2025-08-02 13:58:54 -0700

    Add Terraform Prod

---
+++ terraform/env/prod/variables.tf
@@ -0,0 +1,11 @@
+variable "region" {
+  default = "us-west-1"
+}
+
+variable "aws-profile" {
+  default = "my-profile" # Change to your AWS config profile
+}
+
+variable "environment" {
+  default = "prod"
+}
