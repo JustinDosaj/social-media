df337ef3fe22b57915a2c60b63034cf547524b69
JustinDosaj <justindosaj@gmail.com>
2025-09-11 14:21:17 -0700

    Remove Profile

--- terraform/env/prod/variables.tf
+++ terraform/env/prod/variables.tf
@@ -2,10 +2,6 @@ variable "region" {
   default = "us-west-1"
 }
 
-variable "aws-profile" {
-  default = "my-profile" # Change to your AWS config profile
-}
-
 variable "environment" {
   default = "prod"
 }
