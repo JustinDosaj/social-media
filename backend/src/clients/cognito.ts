5405b6cc3d920f56f04307a6035b109c3ecfd1c5
JustinDosaj <justindosaj@gmail.com>
2025-09-11 12:43:03 -0700

    Remove Comments

--- backend/src/clients/cognito.ts
+++ backend/src/clients/cognito.ts
@@ -4,13 +4,6 @@ import { getParametersFromSSM } from "./ssm";
 
 dotenv.config()
 
-// export const CLIENT_ID = process.env.COGNITO_CLIENT_ID as string
-// export const USER_POOL_ID = process.env.COGNITO_USER_POOL_ID as string
-
-// export const cognitoClient = new CognitoIdentityProviderClient({
-//     region: process.env.COGNITO_REGION as string,
-// })
-
 export let CLIENT_ID: string
 export let USER_POOL_ID: string
 export let REGION: string
