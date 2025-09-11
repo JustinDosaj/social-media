682876a1f05c397b378c10e6f844090826c215a7
JustinDosaj <justindosaj@gmail.com>
2025-09-11 14:29:42 -0700

    New Profile/Reformat

--- terraform/env/dev/variables.tf
+++ terraform/env/dev/variables.tf
@@ -2,6 +2,10 @@ variable "region" {
   default = "us-west-1"
 }
 
+variable "aws-profile" {
+  default = "dev" # Change to your AWS config profile
+}
+
 variable "environment" {
   default = "dev"
 }
