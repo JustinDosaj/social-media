17737db78290c9bd81a8019f10325cb8b1cffc96
JustinDosaj <justindosaj@gmail.com>
2025-09-11 14:49:39 -0700

    Overwrite SSM

--- terraform/modules/ssm/main.tf
+++ terraform/modules/ssm/main.tf
@@ -1,17 +1,20 @@
 resource "aws_ssm_parameter" "cognito_user_pool_id" {
-  name  = "/social-media/${var.environment}/cognito_user_pool_id"
-  type  = "String"
-  value = var.cognito_user_pool_id
+  name      = "/social-media/${var.environment}/cognito_user_pool_id"
+  type      = "String"
+  value     = var.cognito_user_pool_id
+  overwrite = true
 }
 
 resource "aws_ssm_parameter" "cognito_client_id" {
-  name  = "/social-media/${var.environment}/cognito_client_id"
-  type  = "String"
-  value = var.cognito_client_id
+  name      = "/social-media/${var.environment}/cognito_client_id"
+  type      = "String"
+  value     = var.cognito_client_id
+  overwrite = true
 }
 
 resource "aws_ssm_parameter" "cognito_region" {
-  name  = "/social-media/${var.environment}/cognito_region"
-  type  = "String"
-  value = var.region
+  name      = "/social-media/${var.environment}/cognito_region"
+  type      = "String"
+  value     = var.region
+  overwrite = true
 }
